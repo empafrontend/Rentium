304b08f35e16466ba819518e89133aa5ec79f768
Millie Cheung <cheungmillie9@gmail.com>
2022-11-23 16:36:11 +0100

    added mui theme and styled a few elements

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from '@emotion/react';
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AdFilterFeed from './AdFilterFeed';
@@ -6,20 +7,23 @@ import './index.css';
 import LandingPage from './LandingPage';
 import SignIn from './signInAndSignUp/SignIn';
 import SignUp from './signInAndSignUp/SignUp';
+import { theme } from './theme';
 
 function App() {
   return (
-    <React.StrictMode>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/sign-in" element={<SignIn />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/filter" element={<AdFilterFeed />} />
-          <Route path="/feed" element={<MainFeed />} />
-        </Routes>
-      </BrowserRouter>
-    </React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <React.StrictMode>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/sign-in" element={<SignIn />} />
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/filter" element={<AdFilterFeed />} />
+            <Route path="/feed" element={<MainFeed />} />
+          </Routes>
+        </BrowserRouter>
+      </React.StrictMode>
+    </ThemeProvider>
   );
 }
 
