a53607b66a9dd7bbbdccc7aaf1521a3a9d28e011
Rosanna Pistone <rosanna@pistone.se>
2022-11-23 14:30:46 +0100

    added some ads

--- src/Feed/FeedCard.tsx
+++ src/Feed/FeedCard.tsx
@@ -1,7 +1,7 @@
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 import { Box, Card, CardContent, CardMedia, Typography } from '@mui/material';
 import { Container } from '@mui/system';
-import productImg from '../Assets/produktbild.jpg';
+import ads from '../adsData';
 import './feed.css';
 
 function FeedCard() {
@@ -24,110 +24,46 @@ function FeedCard() {
           width: '100%',
         }}
       >
-        <Card
-          className="card"
-          sx={{
-            width: '11rem',
-            height: '14rem',
-            borderRadius: '1rem',
-          }}
-        >
-          <CardMedia
-            component="img"
-            image={productImg}
-            alt="Produktbild"
-            sx={{ borderRadius: '1rem', width: '100%', height: '9rem' }}
-          />
-          <CardContent>
-            <Typography gutterBottom variant="subtitle2">
-              Rubrik
-            </Typography>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-              <Typography variant="caption" sx={{ color: 'grey' }}>
-                <LocationOnOutlinedIcon sx={{ fontSize: '1rem' }} /> Göteborg
+        {ads.map((ads) => (
+          <Card
+            className="card"
+            sx={{
+              width: '11rem',
+              height: '14rem',
+              borderRadius: '1rem',
+            }}
+          >
+            <CardMedia
+              component="img"
+              image={ads.img}
+              alt="Produktbild"
+              sx={{
+                borderRadius: '1rem',
+                width: '100%',
+                height: '9rem',
+                objectFit: 'cover',
+                objectPosition: 'center',
+              }}
+            />
+            <CardContent>
+              <Typography gutterBottom variant="subtitle2">
+                {ads.title}
               </Typography>
-              <Typography variant="caption">Pris: 300kr</Typography>
-            </Box>
-          </CardContent>
-        </Card>
-        <Card
-          className="card"
-          sx={{
-            width: '11rem',
-            height: '14rem',
-            borderRadius: '1rem',
-          }}
-        >
-          <CardMedia
-            component="img"
-            image={productImg}
-            alt="Produktbild"
-            sx={{ borderRadius: '1rem', width: '100%', height: '9rem' }}
-          />
-          <CardContent>
-            <Typography gutterBottom variant="subtitle2">
-              Rubrik
-            </Typography>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-              <Typography variant="caption" sx={{ color: 'grey' }}>
-                <LocationOnOutlinedIcon sx={{ fontSize: '1rem' }} /> Göteborg
-              </Typography>
-              <Typography variant="caption">Pris: 300kr</Typography>
-            </Box>
-          </CardContent>
-        </Card>
-        <Card
-          className="card"
-          sx={{
-            width: '11rem',
-            height: '14rem',
-            borderRadius: '1rem',
-          }}
-        >
-          <CardMedia
-            component="img"
-            image={productImg}
-            alt="Produktbild"
-            sx={{ borderRadius: '1rem', width: '100%', height: '9rem' }}
-          />
-          <CardContent>
-            <Typography gutterBottom variant="subtitle2">
-              Rubrik
-            </Typography>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-              <Typography variant="caption" sx={{ color: 'grey' }}>
-                <LocationOnOutlinedIcon sx={{ fontSize: '1rem' }} /> Göteborg
-              </Typography>
-              <Typography variant="caption">Pris: 300kr</Typography>
-            </Box>
-          </CardContent>
-        </Card>
-        <Card
-          className="card"
-          sx={{
-            width: '11rem',
-            height: '14rem',
-            borderRadius: '1rem',
-          }}
-        >
-          <CardMedia
-            component="img"
-            image={productImg}
-            alt="Produktbild"
-            sx={{ borderRadius: '1rem', width: '100%', height: '9rem' }}
-          />
-          <CardContent>
-            <Typography gutterBottom variant="subtitle2">
-              Rubrik
-            </Typography>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-              <Typography variant="caption" sx={{ color: 'grey' }}>
-                <LocationOnOutlinedIcon sx={{ fontSize: '1rem' }} /> Göteborg
-              </Typography>
-              <Typography variant="caption">Pris: 300kr</Typography>
-            </Box>
-          </CardContent>
-        </Card>
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                }}
+              >
+                <Typography variant="caption" sx={{ color: 'grey' }}>
+                  <LocationOnOutlinedIcon sx={{ fontSize: '.8rem' }} />{' '}
+                  {ads.location}
+                </Typography>
+                <Typography variant="caption">Pris: {ads.price}</Typography>
+              </Box>
+            </CardContent>
+          </Card>
+        ))}
       </Box>
     </Container>
   );
