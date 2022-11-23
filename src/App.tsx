a183706057f22ac919f112be5f9f51a91af75cc2
Rosanna Pistone <rosanna@pistone.se>
2022-11-23 16:44:20 +0100

    Added simple header, prepared for navigation links for larger screens

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AdFilterFeed from './AdFilterFeed';
 import MainFeed from './Feed/Feed';
+import Header from './Header';
 import './index.css';
 import LandingPage from './LandingPage';
 
 function App() {
   return (
     <React.StrictMode>
+      <Header />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
---
+++ src/Header.tsx
@@ -0,0 +1,18 @@
+import { Box } from '@mui/material';
+import './header.css';
+
+function Header() {
+  return (
+    <Box className="header">
+      {/* <Box component="img" src={headerImg} sx={{ width: '8rem' }}></Box> */}
+      {/*    <Box className='navigation'>
+          {HomeOutlinedIcon}
+          {SearchOutlinedIcon}
+          {AddCircleOutlineOutlinedIcon}
+          {PersonOutlineOutlinedIcon}
+      </Box> */}
+    </Box>
+  );
+}
+
+export default Header;
