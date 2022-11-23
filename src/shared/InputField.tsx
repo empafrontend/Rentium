bd3023be7f5e5f26985e91496009de4c4ab730f9
Millie Cheung <cheungmillie9@gmail.com>
2022-11-23 14:26:08 +0100

    created shared components for input and button

--- src/shared/InputField.tsx
+++ src/shared/InputField.tsx
@@ -1,15 +1,44 @@
-import { FormControl, Input, InputLabel } from '@mui/material';
+import { FormControl, TextField } from '@mui/material';
 
 interface Props {
   label: string;
   type: string;
   value: string;
+  error?: boolean;
+  helperText?: string | undefined | boolean;
+  onChange?: any;
 }
 const InputField = (props: Props) => {
   return (
     <FormControl variant="standard">
-      <InputLabel htmlFor={props.type}>{props.label}</InputLabel>
-      <Input required id={props.type} type={props.type} value={props.value} />
+      <TextField
+        variant="standard"
+        label={props.label}
+        id={props.type}
+        type={props.type}
+        value={props.value}
+        error={props.error}
+        helperText={props.helperText}
+        onChange={props.onChange}
+        InputLabelProps={{
+          shrink: true,
+          style: { color: '#535353', fontWeight: 300, paddingLeft: 5 },
+        }}
+        InputProps={{
+          disableUnderline: true,
+          style: {
+            background: '#F5F5F5',
+            borderRadius: 15,
+            height: 35,
+            paddingLeft: 15,
+            paddingRight: 15,
+            fontSize: 13,
+          },
+        }}
+        FormHelperTextProps={{
+          style: { paddingLeft: 5, fontWeight: 300 },
+        }}
+      />
     </FormControl>
   );
 };
