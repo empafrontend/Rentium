e372096203651583e3f3cb8b798f98e8c5df4b9d
Millie Cheung <cheungmillie9@gmail.com>
2022-11-23 14:26:39 +0100

    signin form validation done, but link to no function yet

--- src/signInAndSignUp/SignIn.tsx
+++ src/signInAndSignUp/SignIn.tsx
@@ -1,37 +1,72 @@
-import { Box, Button, Container, Typography } from '@mui/material';
+import { Box, Container, Typography } from '@mui/material';
+import { useFormik } from 'formik';
 import { Link } from 'react-router-dom';
+import * as yup from 'yup';
 import InputField from '../shared/InputField';
+import StyledButton from '../shared/StyledButton';
+
+const validationSchema = yup.object({
+  username: yup
+    .string()
+    .required('Required')
+    .min(3, 'Username must be between 3 to 20 characters')
+    .max(20, 'Username must be between 3 to 20 characters'),
+  password: yup
+    .string()
+    .required('Required')
+    .min(6, 'Password must be at least 6 characters'),
+});
 
 const SignIn = () => {
+  const handleSignIn = (values: any) => console.log('signing in'); // no function yet!
+  const formik = useFormik({
+    initialValues: { username: '', password: '' },
+    validationSchema: validationSchema,
+    validateOnMount: true,
+    onSubmit: (values) => handleSignIn(values),
+  });
+
   return (
     <Container
       sx={{
         display: 'flex',
         flexDirection: 'column',
-        placeItems: 'center',
+        alignItems: 'center',
         rowGap: 5,
       }}
     >
-      Välkommen!
+      Välkommen! {/* to be wrapped with typography once there is theme */}
       <Box
         component="form"
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          maxWidth: 350,
-          minWidth: 300,
-          rowGap: 5,
-        }}
+        maxWidth={350}
+        minWidth={300}
+        margin="auto"
+        sx={{ display: 'flex', flexDirection: 'column', rowGap: 5 }}
+        onSubmit={formik.handleSubmit}
       >
-        <InputField label="Användarnamn" type="username" value="cheungmillie" />
-
-        <InputField label="Lösenord" type="password" value="12345678" />
-        <Button variant="contained">Logga in</Button>
+        <InputField
+          label="Användarnamn"
+          type="username"
+          value={formik.values.username}
+          error={formik.touched.username && Boolean(formik.errors.username)}
+          helperText={formik.touched.username && formik.errors.username}
+          onChange={formik.handleChange}
+        />
+        <InputField
+          label="Lösenord"
+          type="password"
+          value={formik.values.password}
+          error={formik.touched.password && Boolean(formik.errors.password)}
+          helperText={formik.touched.password && formik.errors.password}
+          onChange={formik.handleChange}
+        />
+        <StyledButton content="Logga in" htmlType="submit" />
       </Box>
       <Typography>
         Har du inget konto?{' '}
         <Box component="span" fontWeight={500} sx={{ display: 'inline' }}>
-          <Link to="/sign-up">Registrera dig!</Link>
+          <Link to="/sign-up">Registrera dig!</Link>{' '}
+          {/* to be wrapped with typography once there is theme */}
         </Box>
       </Typography>
     </Container>
