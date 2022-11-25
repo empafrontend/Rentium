67f1ef0033766efbc18f19e7ebec085ea697a4fa
Millie Cheung <cheungmillie9@gmail.com>
2022-11-25 10:07:51 +0100

    created userContext and applied to components for signin and signup

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddPage from './AddPage';
 import AdFilterFeed from './AdFilterFeed';
+import UserProvider from './Context/UserContextProvider';
 import MainFeed from './Feed/Feed';
 import './index.css';
 import Layout from './Layout';
@@ -16,17 +17,19 @@ function App() {
     <ThemeProvider theme={theme}>
       <React.StrictMode>
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Layout />}>
-              <Route index element={<MainFeed />} />
-              <Route path="/sign-in" element={<SignIn />} />
-              <Route path="/sign-up" element={<SignUp />} />
-              <Route path="/my-page" element={<MyPage />} />
-              <Route path="/add" element={<AddPage />} />
-              <Route path="/filter" element={<AdFilterFeed />} />
-              <Route path="/feed" element={<MainFeed />} />
-            </Route>
-          </Routes>
+          <UserProvider>
+            <Routes>
+              <Route path="/" element={<Layout />}>
+                <Route index element={<MainFeed />} />
+                <Route path="/sign-in" element={<SignIn />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/my-page" element={<MyPage />} />
+                <Route path="/add" element={<AddPage />} />
+                <Route path="/filter" element={<AdFilterFeed />} />
+                <Route path="/feed" element={<MainFeed />} />
+              </Route>
+            </Routes>
+          </UserProvider>
         </BrowserRouter>
       </React.StrictMode>
     </ThemeProvider>
