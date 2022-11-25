2495b2a427ccd88b8cb34174f821a8536b4bd0d7
AdrianRydin <adrian.rydin@gmail.com>
2022-11-25 11:14:09 +0100

    Made the LandingPage

--- src/LandingPage.tsx
+++ src/LandingPage.tsx
@@ -1,7 +1,15 @@
 import { Container } from '@mui/material';
+import firebase from 'firebase/compat/app';
+import { firebaseConfig, GoogleLogin } from './firebase';
+
+firebase.initializeApp(firebaseConfig);
 
 function LandingPage() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <button onClick={GoogleLogin}>Logga in med Google</button>
+    </Container>
+  );
 }
 
 export default LandingPage;
