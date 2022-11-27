18c04a519ce20fce0afbe6b6dce6777deb80bc36
saralindqvist <sarathereselindqvist@hotmail.com>
2022-11-27 17:55:48 +0100

    fixed error

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,8 @@ import AdProvider from './Context/AdContextProvider';
 import UserProvider from './Context/UserContextProvider';
 import MainFeed from './Feed/Feed';
 import './index.css';
-import LandingPage from './LandingPage';
-import MyAdd from './MyAdd';
 import Layout from './Layout';
+import MyAdd from './MyAdd';
 import MyPage from './MyPage/MyPage';
 import SignIn from './signInAndSignUp/SignIn';
 import SignUp from './signInAndSignUp/SignUp';
@@ -29,7 +28,7 @@ function App() {
                   <Route path="/sign-up" element={<SignUp />} />
                   <Route path="/my-page" element={<MyPage />} />
                   <Route path="/add" element={<AddPage />} />
-                     <Route path="/my-add" element={<MyAdd />} />
+                  <Route path="/my-add" element={<MyAdd />} />
                   <Route path="/filter" element={<AdFilterFeed />} />
                   <Route path="/feed" element={<MainFeed />} />
                 </Route>
