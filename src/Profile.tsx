41eca6d07f043b1184c053a56753593c818513c7
saralindqvist <sarathereselindqvist@hotmail.com>
2022-11-29 14:31:33 +0100

    adjust styling for the card

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,3 +1,4 @@
+import { LocationOnOutlined } from '@mui/icons-material';
 import { Box, Card, CardContent, CardMedia, Typography } from '@mui/material';
 import ads from './adsData';
 import './footer.css';
@@ -5,61 +6,84 @@ import ContentContainer from './shared/ContentContainer';
 
 function Profile() {
   return (
-    <ContentContainer>
+    <ContentContainer background="#F5F5F5">
       <Box
         sx={{
           display: 'flex',
           flexDirection: 'column',
+          alignItems: 'center',
         }}
       >
-        <Typography component="h2" variant="h4" mb={1} fontWeight={400}>
-          Lindqvistsara's annonser (
-          {ads.filter((ad) => ad.author !== 'lindqvistsara').length}){' '}
-          {/* TODO: filter is in but should change author string to correct user */}
-        </Typography>
-        <Card
+        <Typography
+          variant="h5"
           sx={{
-            borderRadius: '20px 20px 20px 0',
-            boxShadow: 'none',
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            marginLeft: '1rem',
+            paddingTop: '2rem',
+            paddingBottom: '2rem',
           }}
         >
-          {ads
-            .filter((ad) => ad.author !== 'lindqvistsara')
-            .map((ad, index) => (
-              <Box
+          Lindqvistsara's annonser (
+          {ads.filter((ad) => ad.author !== 'lindqvistsara').length}){' '}
+        </Typography>
+
+        {ads
+          .filter((ad) => ad.author !== 'lindqvistsara')
+          .map((ad, index) => (
+            <Card
+              className="card"
+              key={index}
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                pl: 1,
+                width: '60%',
+                height: 120,
+              }}
+            >
+              <CardMedia
+                component="img"
+                image={ad.img}
+                alt="img"
+                sx={{
+                  borderRadius: 3,
+                  width: 100,
+                  height: 100,
+                }}
+              />
+              <CardContent
                 sx={{
                   display: 'flex',
-                  flexDirection: 'row',
-                  mb: 3,
+                  flexDirection: 'column',
+                  width: '100%',
+                  height: 120,
                 }}
               >
-                <CardMedia
-                  component="img"
-                  alt={ad.title}
-                  image={ad.img}
-                  sx={{ borderRadius: 3, width: 100, height: 100, rowGap: 5 }}
-                />
-                <CardContent
+                <Typography variant="body1" fontWeight={600}>
+                  {ad.title}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  {ad.description}
+                </Typography>
+                <Box
                   sx={{
                     display: 'flex',
-                    flexDirection: 'column',
-                    placeContent: 'center',
-                    pl: 1,
+                    flexDirection: 'row',
+                    justifyContent: 'space-between',
+                    pt: 4,
                   }}
                 >
-                  <Typography variant="body1" fontWeight={600}>
-                    {ad.title}
-                  </Typography>
                   <Typography variant="body1" fontWeight={400}>
-                    {ad.price} kr
+                    Pris: {ad.price}
                   </Typography>
-                </CardContent>
-              </Box>
-            ))}
-        </Card>
+                  <Typography variant="body2" sx={{ color: 'grey' }}>
+                    <LocationOnOutlined sx={{ fontSize: '.8rem' }} />{' '}
+                    {ad.location}
+                  </Typography>
+                </Box>
+              </CardContent>
+            </Card>
+          ))}
       </Box>
     </ContentContainer>
   );
