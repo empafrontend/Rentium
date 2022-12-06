8a00cab25e99c2ba75a5f21c7d05c326db2186fb
Emil <emil.hagelin@medieinstitutet.se>
2022-12-06 11:51:53 +0100

    Added text to icons for footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -13,28 +13,40 @@ function Footer() {
   return (
     <Box className="footer">
       <Box className="footer-navigation">
-        <Link to="/">
-          <HomeOutlined className="icon" fontSize="large" />
-        </Link>
-        <SearchOutlined className="icon" fontSize="large" />
-        <Link
-          className={!auth.currentUser ? 'hidden' : 'icon'}
-          to={!auth.currentUser ? '/sign-in' : '/new-ad'}
-        >
-          <AddCircleOutlineRounded fontSize="large" />
-        </Link>
-        <Link to={!auth.currentUser ? '/sign-in' : '/my-page'}>
-          {!auth.currentUser ? (
-            <PersonOutlineOutlined className="icon" fontSize="large" />
-          ) : (
-            <Box
-              component="img"
-              className="img-icon icon:hover"
-              // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-              src={auth.currentUser.photoURL!}
-            />
-          )}
-        </Link>
+        <Box>
+          <Link to="/">
+            <HomeOutlined className="icon" fontSize="large" />
+            <h3>Hem</h3>
+          </Link>
+        </Box>
+        <Box>
+          <SearchOutlined className="icon" fontSize="large" />
+          <h3>Sök</h3>
+        </Box>
+        <Box>
+          <Link
+            className={!auth.currentUser ? 'hidden' : 'icon'}
+            to={!auth.currentUser ? '/sign-in' : '/new-ad'}
+          >
+            <AddCircleOutlineRounded fontSize="large" />
+            <h3>Ny Annons</h3>
+          </Link>
+        </Box>
+        <Box>
+          <Link to={!auth.currentUser ? '/sign-in' : '/my-page'}>
+            {!auth.currentUser ? (
+              <PersonOutlineOutlined className="icon" fontSize="large" />
+            ) : (
+              <Box
+                component="img"
+                className="img-icon icon:hover"
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                src={auth.currentUser.photoURL!}
+              />
+            )}
+            <h3>Min Profil</h3>
+          </Link>
+        </Box>
       </Box>
     </Box>
   );
