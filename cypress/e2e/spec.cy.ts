cad0cde3a88e75a000e596eef9ffc5b3bd67628f
Millie Cheung <cheungmillie9@gmail.com>
2022-12-12 15:55:47 +0100

    added tests based on opponering

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -4,17 +4,72 @@ beforeEach(() => {
   cy.visit('/');
 });
 
-describe('Visit the landing page', () => {
-  it('Page should be accessible and loaded with content', () => {
+describe('Landing page', () => {
+  it('Should be accessible and loaded with content', () => {
     cy.get('header')
       .find('h1')
       .should('exist')
       .and('be.visible')
       .and('contain', 'Välkommen till Rentium');
   });
+  it('Should have no horizontal scrolling', () => {
+    cy.window()
+      .scrollTo('right', { ensureScrollable: false })
+      .its('scrollX')
+      .should('equal', 0);
+  });
+  it('Filter items should contains a text under icon', () => {
+    cy.get('header')
+      .get('button')
+      .first()
+      .siblings()
+      .should('contain', 'Skor')
+      .and('be.visible');
+    cy.get('header')
+      .get('button')
+      .eq(1)
+      .siblings()
+      .should('contain', 'Hattar')
+      .and('be.visible');
+    cy.get('header')
+      .get('button')
+      .eq(2)
+      .siblings()
+      .should('contain', 'Verktyg')
+      .and('be.visible');
+    cy.get('header')
+      .get('button')
+      .eq(3)
+      .siblings()
+      .should('contain', 'Kläder')
+      .and('be.visible');
+    cy.get('header')
+      .get('button')
+      .eq(4)
+      .siblings()
+      .should('contain', 'Fordon')
+      .and('be.visible');
+  });
 });
 
-describe('The Login Page validation', () => {
+describe('Footer', () => {
+  it("Should contain an 'all rights reserved' statement in desktop version", () => {
+    cy.get('footer')
+      .should('exist')
+      .and('contain', 'Alla rättigheter förbehållna');
+  });
+  it('Should contain nav items in mobile version', () => {
+    cy.viewport('iphone-xr', 'portrait');
+    cy.get('footer').find('a').first().should('exist').and('contain', 'Hem');
+    cy.get('footer')
+      .find('a')
+      .last()
+      .should('exist')
+      .and('contain', 'Logga in');
+  });
+});
+
+describe('Login form validation', () => {
   it('Should redirect user to login page when clicking "Logga in"', () => {
     cy.get('header').find('a').contains('Logga in').click();
     cy.url().should('eq', `${Cypress.config('baseUrl')}/sign-in`);
